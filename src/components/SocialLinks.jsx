e91fb479d5b82bdd57321708f4e8582149947511
Sahil <mohdsahil282002@gmail.com>
2025-03-31 16:18:08 +0530

    Fix: Social links

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -16,13 +16,13 @@ export const SocialLinks = () => {
 
             {/* Social Links */}
             <div className="fixed right-22 top-1/2 -translate-y-1/2 flex flex-col gap-4 [@media(max-width:1272px)]:hidden">
-                <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="group transition-transform hover:-translate-y-1">
+                <a href="https://github.com/Sahil2802" target="_blank" rel="noopener noreferrer" className="group transition-transform hover:-translate-y-1">
                     <img src={assets.Github} alt="GitHub" className="w-6 h-6 transition-colors group-hover:brightness-0 group-hover:invert" />
                 </a>
-                <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="group transition-transform hover:-translate-y-1">
+                <a href="https://www.linkedin.com/in/sahil2802/" target="_blank" rel="noopener noreferrer" className="group transition-transform hover:-translate-y-1">
                     <img src={assets.Linkedin} alt="LinkedIn" className="w-6 h-6 transition-colors group-hover:brightness-0 group-hover:invert" />
                 </a>
-                <a href="mailto:your.email@example.com" className="group transition-transform hover:-translate-y-1">
+                <a href="mailto:forsahil@gmail.com" className="group transition-transform hover:-translate-y-1">
                     <img src={assets.Mail} alt="Email" className="w-6 h-6 transition-colors group-hover:brightness-0 group-hover:invert" />
                 </a>
             </div>
